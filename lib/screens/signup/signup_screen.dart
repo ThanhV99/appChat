e83832b800485ae88ee078654e02d31a7d8aa9ec
ThanhV99 <thanhyb1999@gmail.com>
2023-01-06 16:24:30 +0700

    them widget man hinh signup

--- lib/screens/signup/signup_screen.dart
+++ lib/screens/signup/signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Widget/bezierContainer.dart';
+import '../chats/chats_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -10,20 +13,206 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("SIGNUP"),
-        leading: IconButton(
-          onPressed:() => Navigator.pop(context),
-          icon: const Icon(Icons.arrow_back),
+      body: SafeArea(
+        child: Container(
+          height: size.height,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: 40,
+                  left: 0,
+                  child: InkWell(
+                    onTap: (){
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      child: Row(
+                        children: [
+                          Icon(Icons.keyboard_arrow_left),
+                          Text(
+                            "Quay lại",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+              ),
+              Positioned(
+                top: -size.height * .15,
+                right: -MediaQuery.of(context).size.width * .4,
+                child: BezierContainer(),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: size.height * .2),
+                      const SizedBox(height: 20),
+                      Column(
+                        children: <Widget>[
+                          const SizedBox(height: 80),
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 10,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                const Text(
+                                  "Tên hiển thị",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextField(
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                  textAlign: TextAlign.start,
+                                  obscureText: false,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ]
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const Text(
+                              "Số điện thoại",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextField(
+                              style: const TextStyle(
+                                fontSize: 20,
+                              ),
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                              textAlign: TextAlign.start,
+                              obscureText: false,
+                              decoration: InputDecoration(
+                                suffixIcon: const Icon(
+                                  Icons.phone_android,
+                                  color: Colors.black54,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const Text(
+                              "Mật khẩu",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                suffixIcon: const Icon(
+                                  Icons.visibility,
+                                  color: Colors.black54,
+                                ),
+                                // icon: Icon(Icons.lock),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Container(
+                              width: MediaQuery.of(
+                                context,
+                              ).size.width,
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 15,
+                              ),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                boxShadow: <BoxShadow>[
+                                  BoxShadow(
+                                      color: Colors.grey.shade200,
+                                      offset: const Offset(2, 4),
+                                      blurRadius: 5,
+                                      spreadRadius: 2),
+                                ],
+                                gradient: const LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    Colors.blue,
+                                    Colors.lightBlue,
+                                  ],
+                                ),
+                              ),
+                              child: InkWell(
+                                onTap: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => ChatsScreen()));
+                                },
+                                child: const Text(
+                                  'Đăng kí',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              )
+            ],
+          ),
         ),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("signup screen"),
-        ),
-      ),
+      )
     );
   }
 }
